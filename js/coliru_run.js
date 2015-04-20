f5329469d544a5b9cf5ed2f0453b8a4a1c5abf58
Mauro <essgeeeich@gmail.com>
2015-04-20 20:37:10 +0200

    #5 fixed

--- js/coliru_run.js
+++ js/coliru_run.js
@@ -1,3 +1,2 @@
 var data=function(){var query_string={};var query=window.location.search.substring(1);var vars=query.split("&");for(var i=0;i<vars.length;i++){var pair=vars[i].split("=");if(typeof query_string[pair[0]]==="undefined"){query_string[pair[0]]=pair[1];}else if(typeof query_string[pair[0]]==="string"){var arr=[query_string[pair[0]],pair[1]];query_string[pair[0]]=arr;}else{query_string[pair[0]].push(pair[1]);}}return query_string;}();
 if(data.src) localStorage.setItem('src',unescape(data.src));
-else localStorage.removeItem('src');
